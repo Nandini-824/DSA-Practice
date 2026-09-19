9b501741251f0436b89994d40a9469b2c974ac9f
Nandini <nap55125@gmail.com>
2026-09-20 04:58:03 +0530

    Time: 2 ms (43.77%), Space: 11.9 MB (18.06%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> ans;
+
+        int count3=0, count5=0;
+        for(int i=1;i<=n;i++){
+            count3++;
+            count5++;
+            if(count3==3 && count5==5){
+                ans.push_back("FizzBuzz");
+                count3=0;
+                count5=0;
+            }
+            else if(count3==3){
+                ans.push_back("Fizz");
+                count3=0;
+            }
+            else if(count5==5){
+                ans.push_back("Buzz");
+                count5=0;
+            }
+            else{
+                ans.push_back(to_string(i));
+            }
+
+        }
+
+        return ans;
+        
+    }
+};
